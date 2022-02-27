7c695f62f3a61ce29cb27d33070e0d6dcfba9bf5
Gopalkrishna Veni <gveni@ancestry.com>
2022-02-27 08:02:02 -0700

    finished implementing dcgan

--- pytorch_tutorial/dcgan_celeba.py
+++ pytorch_tutorial/dcgan_celeba.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import sys
 import random
 import matplotlib.pyplot as plt
 import numpy as np
@@ -28,7 +29,6 @@ num_epochs = 5
 lr = 0.0002
 beta1 = 0.5  # initial decay rate of ADAM optimizer for first moment of gradient
 ngpu = 1
-
 
 # create a dataset  and dataloader class
 dataset = dataset.ImageFolder(root=dataroot,
@@ -130,3 +130,79 @@ class Discriminator(nn.Module):
 netD = Discriminator(ngpu).to(device)
 netD.apply(weights_init)
 print(netD)  # print discriminator model architecture
+
+# define losses, real, fake label conventions and optimizers
+loss_criteria = nn.BCELoss()
+real_label = 1.
+fake_label = 0.
+optimizerG = torch.optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
+optimizerD = torch.optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
+
+# Create a batch of latent vectors
+fixed_noise = torch.randn(64, nz, 1, 1, device=device)
+
+print("Training DCGAN on celebA dataset...")
+lossG_progress = []
+lossD_progress = []
+img_list = []
+
+for epoch in range(num_epochs):
+    for i, data in enumerate(dataloader):
+        # Train Discriminator: maximize log(D(x)) + log(1-(D(G(z))))
+        # 1. on real examples
+        # clear discriminator gradients
+        netD.zero_grad()
+        real_batch = data[0].to(device)
+        batch_sz = real_batch.size(0)
+        label = torch.full((batch_sz,), real_label, dtype=torch.float, device=device)
+        # forward pass real example batch through discriminator
+        output = netD(real_batch)
+        # calculate discriminator loss
+        lossD_real = loss_criteria(output, label)
+        # backpropagate loss to compute gradients
+        lossD_real.backward()
+        D_x = output.mean().item()
+
+        # 2. on fake examples
+        # generate a batch of latent vectors
+        noise = torch.randn(batch_sz, nz, 1, 1, device=device)
+        # generate fake images
+        fake_batch = netG(noise)
+        label.fill_(fake_label)
+        # forward pass fake batch through discriminator
+        output = netD(fake_batch)
+        # calculate discriminator loss
+        lossD_fake = loss_criteria(output, label)
+        # backpropagate loss
+        lossD_fake.backward()
+        D_G_z1 = output.mean().item()
+        # compute D by summing D_real and D_fake losses
+        loss_D = lossD_real + lossD_fake
+        # optimize D by updating weights
+        optimizerD.step()
+
+        # Train Generator: maximize log(D(G(z)))
+        netG.zero_grad()
+        label.fill_(real_label)  # fake labels are real for generator cost
+        # Since D is updated,
+        output = netD(fake_batch)
+        # Calculate G's loss based on this output
+        loss_G = loss_criteria(output, label)
+        # backpropagate loss
+        loss_G.backward()
+        D_G_z2 = output.mean().item()
+        # optimize G by updating weights
+        optimizerG.step()
+
+        if i % 50 == 0:
+            print("[%d/%d][%d/%d]\tLoss_D: %.3f\tLoss_G: %.3f\tD_x: %.3f\tD_G_z1: %.3f\t D_G_z2: %.3f)"
+                  % (epoch, num_epochs, i, len(dataloader), loss_D.item(), loss_G.ite(), D_x, D_G_z1, D_G_z2))
+
+        # save losses progress
+        lossG_progress.append(loss_G.item())
+        lossD_progress.append(loss_D.item())
+
+    # Track generator output at regular intervals
+    with torch.no_grad():
+        fake_output = netG(fixed_noise).detach().cpu()
+        img_list.append(vutils.make_grid(fake_output, padding=2, normalize=True))
