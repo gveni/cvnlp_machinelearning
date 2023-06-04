42f94ec161f7c8543e8290fc1b69b40530970a40
Gopalkrishna Veni <gveni@ancestry.com>
2023-06-04 10:27:31 -0600

    added a few print statements in tokenizer

--- transformers_tutorial/tokenizers.py
+++ transformers_tutorial/tokenizers.py
@@ -1,4 +1,4 @@
-
+import sys
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
@@ -15,16 +15,19 @@ tokenizer(input_text_seq1)
 # elaborative tokenization approach (two steps: (1) convert to tokens, (2) extract token-IDs from model vocabulary)
 token1 = tokenizer.tokenize(input_text_seq1)
 token1_id = tokenizer.convert_token_to_ids(token1)
+print("Number of tokens in token1", len(token1_id))
 token2 = tokenizer.tokenize(input_text_seq2)
 token2_id = tokenizer.convert_token_to_ids(token2)
+print("Number of tokens in token2", len(token2_id))
 token3 = tokenizer.tokenize(input_text_seq3)
 token3_id = tokenizer.convert_token_to_ids(token3)
+print("Number of tokens in token3", len(token3_id))
 
-input_ids = torch.tensor([token_id])
-print("Input IDs:", token_ids)
+#input_ids = torch.tensor([token_id])
+#print("Input IDs:", token_ids)
 
 # decoding (converting token-IDs back to input text sequence)
 #output_text_seq = bert_tokenizer.decode(token_ids)
 #print(output_text_seq)
-model_output = model(input_ids)
-print("Logits:", model_output.logits)
+#model_output = model(input_ids)
+#print("Logits:", model_output.logits)
