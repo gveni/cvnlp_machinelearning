7e28def0d58d00b63c5f6c07fb096e4341b2e2d8
Gopalkrishna Veni <gveni@ancestry.com>
2023-11-13 22:22:18 -0700

    added digit and punctuation in gender removal logic in gender classify

--- transformers_tutorial/gender_classification.py
+++ transformers_tutorial/gender_classification.py
@@ -1,13 +1,18 @@
 # Load model directly
+import re
+from string import punctuation
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-
-text = "Jemmimah"
+text = "(John3,"
+text_nonum = re.sub(r'[0-9]', '', text)
+text_nopunc = text_nonum.strip(punctuation)
+print(text_nopunc)
 tokenizer = AutoTokenizer.from_pretrained("padmajabfrl/Gender-Classification")
-inputs = tokenizer(text, return_tensors="pt")
+inputs = tokenizer(text_nopunc, return_tensors="pt")
 model = AutoModelForSequenceClassification.from_pretrained("padmajabfrl/Gender-Classification")
 with torch.no_grad():
     logits = model(**inputs).logits
 predicted_gender = logits.argmax().item()
-print(str(text), "is", model.config.id2label[predicted_gender])
+predicted_gender_score = torch.softmax(logits, dim=1).numpy().max()
+print(str(text_nopunc), "is", model.config.id2label[predicted_gender], "and its score is", predicted_gender_score)
